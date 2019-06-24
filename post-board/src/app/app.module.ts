f42d3cb024ea578091ccba49523a3a1abb0c6bf1
yinon <yinon.bentarya@gmail.com>
2019-06-24 23:35:05 +0300

    embed angular material

--- post-board/src/app/app.module.ts
+++ post-board/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { PostItemListComponent } from './components/post-item-list/post-item-lis
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule
   ],
   providers: [],
